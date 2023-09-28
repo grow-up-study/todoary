d7f0f218a0a0cd1d27d6b6589f51f9b6a3587c8d
daheeParkk <qkrekgml7414@naver.com>
2023-09-28 21:56:46 +0900

    init commit

---
+++ src/main/java/com/growup/todoary/TodoaryApplication.java
@@ -0,0 +1,13 @@
+package com.growup.todoary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoaryApplication.class, args);
+	}
+
+}
