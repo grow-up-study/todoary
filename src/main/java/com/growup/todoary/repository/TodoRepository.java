eef6cece5a93ad3a3f4d2d8c59011c726f86543a
daheeParkk <qkrekgml7414@naver.com>
2023-09-29 01:04:19 +0900

    feat : todo repository 생성

--- src/main/java/com/growup/todoary/repository/TodoRepository.java
+++ src/main/java/com/growup/todoary/repository/TodoRepository.java
@@ -1,2 +1,20 @@
-package com.growup.todoary.repository;public class TodoRepository {
+package com.growup.todoary.repository;
+
+import com.growup.todoary.domain.Todo;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoRepository {
+
+    private final EntityManager em;
+
+    public List<Todo> getTodos() {
+        return em.createQuery("SELECT t FROM Todo t", Todo.class)
+                .getResultList();
+    }
 }
