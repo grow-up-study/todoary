9f13cee977f14419c05d9ec5e76b368698c35573
daheeParkk <qkrekgml7414@naver.com>
2023-09-30 17:08:28 +0900

    refactor : @controller에서 @restController로 변경

--- src/main/java/com/growup/todoary/controller/TodoController.java
+++ src/main/java/com/growup/todoary/controller/TodoController.java
@@ -5,12 +5,12 @@ import com.growup.todoary.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class TodoController {
 
