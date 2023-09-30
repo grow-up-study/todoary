2283c79faf91794dc9891a5a98dfd1cbfa45e075
daheeParkk <qkrekgml7414@naver.com>
2023-09-30 17:36:45 +0900

    refactor : 안 쓰는 import 제거

--- src/main/java/com/growup/todoary/controller/TodoController.java
+++ src/main/java/com/growup/todoary/controller/TodoController.java
@@ -3,7 +3,6 @@ package com.growup.todoary.controller;
 import com.growup.todoary.domain.Todo;
 import com.growup.todoary.service.TodoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
