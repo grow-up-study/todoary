e4e19dfca4270882748d12a26bbd93caa84f848c
daheeParkk <qkrekgml7414@naver.com>
2023-10-03 20:07:17 +0900

    refactor : Todo와 Category의 연관관계 생성

--- src/main/java/com/growup/todoary/domain/Category.java
+++ src/main/java/com/growup/todoary/domain/Category.java
@@ -5,16 +5,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 public class Category {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
     private long id;
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Todo> todos = new ArrayList<>();
 }
