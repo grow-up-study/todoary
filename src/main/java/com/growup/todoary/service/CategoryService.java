eeeb348e1ccb596dd2c52b9a58873ce75d738ab9
daheeParkk <qkrekgml7414@naver.com>
2023-10-03 20:22:07 +0900

    feat : category service 생성

--- src/main/java/com/growup/todoary/service/CategoryService.java
+++ src/main/java/com/growup/todoary/service/CategoryService.java
@@ -1,2 +1,26 @@
-package com.growup.todoary.service;public class CategoryService {
+package com.growup.todoary.service;
+
+import com.growup.todoary.domain.Category;
+import com.growup.todoary.repository.CategoryRepository;
+import com.growup.todoary.service.dto.response.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(CategoryResponse::new)
+                .collect(Collectors.toList());
+    }
 }
