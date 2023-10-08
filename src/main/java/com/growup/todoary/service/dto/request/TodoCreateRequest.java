1e02fc26b89197f013b7a83aa5c1b36a0865235f
daheeParkk <qkrekgml7414@naver.com>
2023-10-09 02:22:33 +0900

    fix : Todo request DTO에 @NoArgsConstructor 추가

--- src/main/java/com/growup/todoary/service/dto/request/TodoCreateRequest.java
+++ src/main/java/com/growup/todoary/service/dto/request/TodoCreateRequest.java
@@ -1,21 +1,23 @@
 package com.growup.todoary.service.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoCreateRequest {
 
     @NotNull(message = "name은 null이면 안됩니다.")
-    private final String name;
+    private String name;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "date는 null이면 안됩니다.")
-    private final LocalDate date;
+    private LocalDate date;
 }
